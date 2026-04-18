2eaf490462ae591cce89366cfa41cf6a09bc1983
Malath A. <malathas14@gmail.com>
2026-04-19 00:04:17 +0300

    fix: correct TLS cert SAN and webhook caBundle configuration

--- certs/gen-certs.sh
+++ certs/gen-certs.sh
@@ -1,25 +1,22 @@
-#!/bin/bash
-SERVICE=webhook-service
-NAMESPACE=default
-SECRET=webhook-tls
-
-# Generate CA key and certificate
-openssl genrsa -out ca.key 2048
-openssl req -new -x509 -days 365 -key ca.key -subj "/CN=webhook-ca" -out ca.crt
-
-
+# Generate server key and certificate with SAN
 openssl genrsa -out tls.key 2048
-openssl req -new -key tls.key -subj "/CN=${SERVICE}.${NAMESPACE}.svc" -out tls.csr
-openssl x509 -req -days 365 -in tls.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out tls.crt
+openssl req -new -key tls.key \
+  -subj "/CN=${SERVICE}.${NAMESPACE}.svc" \
+  -out tls.csr
 
+# Create SAN config
+cat > san.cnf <<EOF
+[req]
+req_extensions = v3_req
+[v3_req]
+subjectAltName = DNS:${SERVICE}.${NAMESPACE}.svc,DNS:${SERVICE}.${NAMESPACE}.svc.cluster.local
+EOF
 
-
-kubectl create secret generic ${SECRET} \
-  --from-file=tls.crt=tls.crt \
-  --from-file=tls.key=tls.key \
-  --namespace=${NAMESPACE}
-
-
-echo "CA Bundle (use this in webhook configuration):"
-cat ca.crt | base64 | tr -d '\n'
-echo ""
+openssl x509 -req -days 365 \
+  -in tls.csr \
+  -CA ca.crt \
+  -CAkey ca.key \
+  -CAcreateserial \
+  -extensions v3_req \
+  -extfile san.cnf \
+  -out tls.crt
